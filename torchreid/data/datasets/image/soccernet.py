18f4f35daaa4e33240886e9f5b53b06c350e5ac2
amir <42087527+AmirMansurian@users.noreply.github.com>
2023-01-09 12:34:39 +0100

    Update soccernet.py

--- torchreid/data/datasets/image/soccernet.py
+++ torchreid/data/datasets/image/soccernet.py
@@ -4,14 +4,10 @@ import os.path as osp
 import os
 import numpy as np
 import time
-side_map_train = ['060', '061', '062', '063', '064', '065', '066', '067','068', '069', '070', '071', '072', '073', '074', '075',
-            '076', '077', '097', '098', '099', '100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '110',
-            '111', '112', '113', '114', '115', '151', '152', '153', '154', '155', '156', '157', '158', '159', '160', '161',
-            '162', '163', '164', '165', '166', '167', '168', '169', '170']
+from configparser import ConfigParser
 
-side_map_test = ['116', '117', '118', '119', '120', '121', '122', '123', '124', '125', '126', '127', '128', '129', '130', '131',
-                 '132', '133', '134', '135', '136', '137', '138', '139', '140', '141', '142', '143', '144', '145', '146', '147',
-                 '148', '149', '150', '187', '188', '189', '190', '191', '192', '193', '194', '195', '196', '197', '198', '199', '200']
+side_map_train = ['4', '6', '9']
+side_map_test = ['7', '8', '11']
 
 from ..dataset import ImageDataset
 
@@ -22,7 +18,6 @@ class SoccerNet(ImageDataset):
     dataset_dir = 'reid_dataset'
     dataset_url = None
     masks_base_dir = 'masks'
-    #eval_metric = 'soccernetv3'
 
     masks_dirs = {
         # dir_name: (masks_stack_size, contains_background_mask)
@@ -94,33 +89,26 @@ class SoccerNet(ImageDataset):
                 camid = img_path.split('/')[-1][:-4].split('_')[1]
                 game_id = img_path.split('/')[-1][:-4].split('_')[-2]
                 role, side = img_path.split('/')[-1][:-4].split('_')[-1].split('-')
-               
-                if role != 'player': continue        
-                if side == 'right':
-                    if mode == 1: pid = side_map_train.index(camid) * 2
-                    elif mode == 2: pid = side_map_test.index(camid) * 2
-                    else:
-                        if camid == '137' : continue
-                        else : pid = side_map_test.index(camid) * 2
-                elif side == 'left':
-                    if mode == 1: pid = side_map_train.index(camid) * 2 + 1
-                    elif mode == 2: pid = side_map_test.index(camid) * 2 + 1
-                    else:
-                        if camid == '137':continue
-                        else: pid = side_map_test.index(camid) * 2 + 1
-                else: continue
+
+                if role != 'player': continue
+
+                team_id = 0
+                if side == 'right': team_id = 1
+                if mode == 1: team_id += side_map_train.index(game_id) * 2
+                else: team_id += side_map_test.index(game_id) * 2
 
                 masks_path = self.infer_masks_path(img_path)
 
                 if (int(pid)) not in id_dict.keys():
                     id_dict[int(pid)] = []
 
-                id_dict[int(pid)].append({'img_path': img_path, 'pid': int(pid), 'masks_path': masks_path, 'camid': int(camid)})
+                id_dict[int(pid)].append({'img_path': img_path, 'pid': int(pid), 'masks_path': masks_path, 'camid': int(camid), 'team_id': team_id})
 
                 data.append({'img_path': img_path,
                                 'pid': int(pid),
                                 'masks_path': masks_path,
-                                'camid': int(camid)})
+                                'camid': int(camid),
+                                'team_id': team_id})
 
         if mode == 1:
             indice = list(set([i['pid'] for i in data]))
@@ -135,16 +123,28 @@ class SoccerNet(ImageDataset):
             return data2, indice
 
         elif mode == 2:
-            indice = list(set([i['pid'] for i in data]))
+            ids = list(set([i['pid'] for i in data]))
+            index = np.random.permutation(len(ids))[:350]
+            indice = [ids[j] for j in index]
             for player in indice:
-                idx = np.random.permutation(len(id_dict[player]))[:30]
+                idx = np.random.permutation(len(id_dict[player]))[:10]
                 data2 += [id_dict[player][f] for f in idx]
+
+            for i, player in enumerate(data2):
+                idx = indice.index(player['pid'])
+                data2[i]['pid'] = idx
 
             return data2, indice
 
         elif mode == 3:
             indice = list(set([i['pid'] for i in data]))
+            indice = [i for i in indice if i in mapping]
             for player in indice:
-                data2 += id_dict[player]
+                idx = np.random.permutation(len(id_dict[player]))
+                data2 += [id_dict[player][f] for f in idx]
+
+            for i, player in enumerate(data2):
+                idx = mapping.index(player['pid'])
+                data2[i]['pid'] = idx
 
             return data2, None
